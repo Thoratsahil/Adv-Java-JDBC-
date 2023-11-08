a5297ecfa772a119a62d9a5555e29efb6f3b1bfa
Thoratsahil <stsahil1717@gmail.com>
2023-11-08 23:10:51 +0530

    save2

--- JdbcConnect.java
+++ JdbcConnect.java
@@ -1,10 +1,19 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class JdbcConnect 
 {
     public static void main( String[] args )
     {
+		Connection con=null; 
         try {
+
 			Class.forName("com.mysql.cj.jdbc.Driver.class");
-		} catch (ClassNotFoundException e) {
+			con=DriverManager.getConnection("jdbc:mysql://localhost:3306/dbconnectivitypractice", "root", null); //This is second step connection
+
+
+		} catch (ClassNotFoundException | SQLException e) {
 			// 1st step load the driver
 			e.printStackTrace();
 		}
